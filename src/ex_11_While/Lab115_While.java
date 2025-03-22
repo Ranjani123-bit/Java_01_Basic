bb83f1cb4b6dff8570ed616810d05de1491f811c
Ranjani <ranjanimalavade73@gmail.com>
2025-03-22 17:40:57 +0530

    Commit 001

---
+++ src/ex_11_While/Lab115_While.java
@@ -0,0 +1,25 @@
+package ex_11_While;
+
+public class Lab115_While {
+    public static void main(String[] args)
+    {
+int i=0 ;
+while(i<10)
+{
+    System.out.println(i);
+    i++;
+}
+    }
+}
+/* Out Put
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+ */
---
+++ src/ex_12_DoWhile/Lab126_diff_while_DoWhile.java
@@ -0,0 +1,27 @@
+package ex_12_DoWhile;
+
+public class Lab126_diff_while_DoWhile {
+
+    public static void main(String[] main)
+    {
+        int a=0;
+       /* while(a>10)
+        {
+            System.out.println("a");
+            a++;  */
+
+        do {
+        System.out.println(a);
+        a++;
+    }
+    while(a>10);
+
+    }
+}
+
+/*  out put
+0
+
+*/
+
+ */
